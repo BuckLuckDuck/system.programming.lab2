3b44087eccd63412e9293db82784652879b81117
BuckLuckDuck <nikitos80011@gmail.com>
2022-05-22 13:12:38 +0700

    Добавьте файлы проекта.

---
+++ System Programming Lab 2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int sortBubble(int *arr, int len)
+{
+    _asm
+    {
+        mov ebx, arr
+        mov ecx, len
+        for_i: dec ecx
+        xor edx, edx
+        for_j: cmp ebx, ecx
+        jae exit_for_j
+        jbe no_swap
+        mov eax, 
+    }
+    return;
+}
+
+int main()
+{
+    const int size = 10;
+    int numbers[size];// { 0 };
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = 9 - i;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
+
+    cout << sortBubble(numbers, 7);
+
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
+}
